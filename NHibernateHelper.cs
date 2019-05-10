87e1f47fbeedd884e7d6a0d9322a05d362d1493a
carlos <carlitoslopezs@hotmail.com>
2019-05-10 18:16:31 -0300

    1.0

---
+++ NHibernateHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+using System.Data.SqlClient;
+using NHibernate.Driver;
+
+namespace TP2
+{
+    class NHibernateHelper
+    {
+        private static ISessionFactory _sessionFactory;
+        private static ISessionFactory SessionFactory
+        {
+            get
+            {
+                if (_sessionFactory == null)
+                    InitializeSessionFactory();
+
+
+                    return _sessionFactory;
+                
+            }
+        }
+        private static void InitializeSessionFactory()
+        {
+            _sessionFactory = Fluently.Configure()
+                .Database(MsSqlConfiguration.MsSql7
+                .ConnectionString(@"Server=(localdb)\mssqllocaldb;Database=TP2;Trusted_Connection=True;")
+                .ShowSql())
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<Car>())
+                .ExposeConfiguration(cfg => new SchemaExport(cfg)
+                .Create(true, false))
+                .BuildSessionFactory();
+        }
+        public static ISession OpenSession()
+        {
+            return SessionFactory.OpenSession();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,122 @@
+﻿using FluentNHibernate.Mapping;
+using System;
+
+namespace TP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var session = NHibernateHelper.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    // ALTA
+                    //Console.WriteLine("Esciba Nombre marca");                                                              
+                    //var fordMake = new Make
+                    //{
+                    //    Name = Console.ReadLine() 
+                    //};
+                    //Console.WriteLine("Esciba Nombre Modelo");
+                    //var fiestaModel = new Model
+                    //{
+                    //    Name = Console.ReadLine(),
+                    //    Make = fordMake
+                    //};
+                    //var car = new Car
+                    //{
+                    //    Make = fordMake,
+                    //    Model = fiestaModel,
+                    //    Title = "Carlos Car"
+                    //};
+                    //session.Save(car);
+                    //transaction.Commit();
+                    //Console.WriteLine("Create Car" + car.Title);
+                   
+                    //BUSQUEDA
+                    Console.WriteLine("Escriba titulo de auto a buscar");
+                    string tit = Console.ReadLine();
+                    Car Cars = null;
+                    var makefound = session.QueryOver<Car>()
+                        .Where(p => p.Title == tit).List();
+                    if (makefound == null)
+                    {
+                        Console.WriteLine("Ningun Auto se llama con ese titulo");
+                    }
+                    else
+                    {
+                        Cars = makefound[0];
+                        Console.WriteLine(Cars.Id);
+                        Console.WriteLine(Cars.Title);
+                    }
+                    // BAJA
+
+                    //session.Delete(Cars);
+                    //transaction.Commit();
+                    //Console.WriteLine("Se Borro");
+
+                    // MODIFICACION
+                    Console.WriteLine("Escriba Titulo de Auto a Cambiar");
+                    Cars.Title = Console.ReadLine();
+                    transaction.Commit();
+                    Console.WriteLine("Se cambio el titulo");
+
+
+
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+    public class MakeMap : ClassMap<Make>
+    {
+        public MakeMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.Name);
+        }
+    }
+
+    public class ModelMap : ClassMap<Model>
+    {
+        public ModelMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.Name);
+            References(x => x.Make).Cascade.All();
+        }
+    }
+
+    public class CarMap : ClassMap<Car>
+    {
+        public CarMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.Title);
+            References(x => x.Make).Cascade.All();
+            References(x => x.Model).Cascade.All();
+        }
+    }
+    public class Make
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+
+    }
+    public class Model
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual Make Make { get; set; }
+
+    }
+    public class Car
+    {
+        public virtual int Id { get; set; }
+        public virtual string Title { get; set; }
+        public virtual Make Make { get; set; }
+        public virtual Model Model { get; set; }
+    }
+
+}
